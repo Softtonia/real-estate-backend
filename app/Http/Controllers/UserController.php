c6b4e86ab6c98b7aa07451e9701ab5495759cc27
JoySofttonia <faiyyaj.alam@softtonia.com>
2025-07-29 16:22:54 +0530

    add user_name in userlisting api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1079,6 +1079,7 @@ class UserController extends Controller
                     'id' => $user->id,
                     'first_name' => $user->first_name,
                     'last_name' => $user->last_name,
+                    'user_name' => $user->user_name,
                     'email' => $user->email,
                     'phone' => $user->phone,
                     'role_name' => $roleName,
