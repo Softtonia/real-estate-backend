5da5a4d5eea8b122b8e1c70f712664befe91d187
JoySofttonia <faiyyaj.alam@softtonia.com>
2025-07-31 19:16:07 +0530

    update ticket status seeder

--- database/seeders/TicketStatusTableSeeder.php
+++ database/seeders/TicketStatusTableSeeder.php
@@ -19,7 +19,7 @@ class TicketStatusTableSeeder extends Seeder
             [
 
                 'icon_id' => 7,
-                'ticket_status_name' => 'Open',
+                'ticket_status_name' => 'New',
                 'display_order' => 1,
                 'created_at' => $now,
                 'updated_at' => $now,
@@ -27,7 +27,7 @@ class TicketStatusTableSeeder extends Seeder
             [
 
                 'icon_id' => 10,
-                'ticket_status_name' => 'Close',
+                'ticket_status_name' => 'In Progress',
                 'display_order' => 2,
                 'created_at' => $now,
                 'updated_at' => $now,
@@ -35,7 +35,7 @@ class TicketStatusTableSeeder extends Seeder
             [
 
                 'icon_id' => 7,
-                'ticket_status_name' => 'New',
+                'ticket_status_name' => 'On Hold',
                 'display_order' => 3,
                 'created_at' => $now,
                 'updated_at' => $now,
@@ -43,7 +43,7 @@ class TicketStatusTableSeeder extends Seeder
             [
 
                 'icon_id' => 8,
-                'ticket_status_name' => 'On Hold',
+                'ticket_status_name' => 'Resolved',
                 'display_order' => 4,
                 'created_at' => $now,
                 'updated_at' => $now,
@@ -51,27 +51,12 @@ class TicketStatusTableSeeder extends Seeder
             [
 
                 'icon_id' => 9,
-                'ticket_status_name' => 'Solved',
+                'ticket_status_name' => 'Closed',
                 'display_order' => 5,
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
-            [
 
-                'icon_id' => 8,
-                'ticket_status_name' => 'Unresolved',
-                'display_order' => 6,
-                'created_at' => $now,
-                'updated_at' => $now,
-            ],
-            [
-
-                'icon_id' => 9,
-                'ticket_status_name' => 'Pending',
-                'display_order' => 7,
-                'created_at' => $now,
-                'updated_at' => $now,
-            ],
         ];
 
         DB::table('ticket_status')->insert($data);
