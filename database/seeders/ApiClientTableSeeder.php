c5300b903882a9a86f8df5adebe7a1cd107fd1d7
JoySofttonia <faiyyaj.alam@softtonia.com>
2025-09-20 17:48:09 +0530

    api client table seeder update data by developer

--- database/seeders/ApiClientTableSeeder.php
+++ database/seeders/ApiClientTableSeeder.php
@@ -30,44 +30,82 @@ class ApiClientTableSeeder extends Seeder
         $clients = [
             [
                 'client_name'     => 'Admin Panel',
-                'client_id'       => 'ADMINCLIENT001',
-                'client_secret'   => 'ADMINSECRET001',
+                'client_id'       => 'OPUVVNR3XNCXPDL',
+                'client_secret'   => '6CNWWJNWAQFO95D',
                 'app_type'        => 'admin',
                 'status'          => '1',
                 'allowed_domain'  => json_encode([
                     "https://admin.urbanrealities.com",
                     "http://admin.urbanrealities.com",
-                    "http://127.0.0.1:8000",
-                    "http://localhost:5173","http://localhost:3000","https://api.urbanrealities.com/public"
                 ]),
             ],
             [
-                'client_name'     => 'Business App',
-                'client_id'       => 'BUSINESSCLI001',
-                'client_secret'   => 'BUSINESSSEC001',
+                'client_name'     => 'API Key Port :5173',
+                'client_id'       => '8CTPOXSMXTWDJIK',
+                'client_secret'   => 'EYOY2ERCWHJ4KW7',
+                'app_type'        => 'admin',
+                'status'          => '1',
+                'allowed_domain'  => json_encode([
+                    "http://localhost:5173"
+                ]),
+            ],
+            [
+                'client_name'     => 'API Key Port : 8000',
+                'client_id'       => 'PX3DUI1NBRQCTGS',
+                'client_secret'   => 'R6LAWNCAACQP27R',
+                'app_type'        => 'admin',
+                'status'          => '1',
+                'allowed_domain'  => json_encode([
+                    "http://127.0.0.1:8000",
+                ]),
+            ],
+            [
+                'client_name'     => 'Business.com',
+                 'client_id'       => 'IFMODQP8ZZOAUU2',   // 15 chars
+                'client_secret'   => 'E9GUFBVPLCTTZRL',
                 'app_type'        => 'business',
                 'status'          => '1',
                 'allowed_domain'  => json_encode([
                     "https://business.urbanrealities.com",
-                    "http://127.0.0.1:8000",
-                    "http://localhost:5173","http://localhost:3000","https://api.urbanrealities.com/public"
+                "http://business.urbanrealities.com"
                 ]),
 
             ],
             [
-                'client_name'     => 'Website Frontend',
-                'client_id'       => 'WEBSITECLI001',
-                'client_secret'   => 'WEBSITESEC001',
+                'client_name'     => 'Business Localhost API Key Port :5173',
+                 'client_id'       => 'ZAYYL8IQDWCRAAZ',   // 15 chars
+                'client_secret'   => 'EGFFBPEYXGDHP5R',
+                'app_type'        => 'business',
+                'status'          => '1',
+                'allowed_domain'  => json_encode([
+                   "http://localhost:5173"
+                ]),
+
+            ],
+            [
+                'client_name'     => 'API Key : 3000',
+                'client_id'       => 'LD5TNUNOKREYBI2',
+                'client_secret'   => '8UJIIPRSUJK4NNI',
+                'app_type'        => 'website',
+                'status'          => '1',
+                'allowed_domain'  => json_encode([
+                    "http://localhost:3000"
+                ]),
+                'nextjs_internal_key' => "XS7N2XMQNAXKMM0XYYWF1EGGZEWHHWXAYQPGX8RV1YNXHHLR1D",
+
+            ],
+            [
+                'client_name'     => 'Urbanrealities.com',
+                'client_id'       => 'KWTWGGSBIZGD7GZ',
+                'client_secret'   => 'DLERJBYZ6QZCW0U',
                 'app_type'        => 'website',
                 'status'          => '1',
                 'allowed_domain'  => json_encode([
                     "https://www.urbanrealities.com",
                     "https://urbanrealities.com",
-                    "http://localhost:5173",
-                    "http://localhost:3000",
-                    "https://api.urbanrealities.com/public"
+                    "http://urbanrealities.com"
                 ]),
-                'nextjs_internal_key' =>"HJQ7CHRZOX1EO3WRUA0ONESEQLQBDECMG5FNUFOKID4WTZGUMG",
+                'nextjs_internal_key' =>"PMCVGOEQZQQUNZODZTNKXAQC10QYLW04HAF316DEDXD7YWD5VR",
             ],
             [
                 'client_name'     => 'Mobile Application',
