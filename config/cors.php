894e10df41c99e5b0d642673907e8d4ccc1489ae
vkwebdev <vijay.kumar@softtonia.com>
2026-07-01 11:35:02 +0530

    api client fixes '

--- config/cors.php
+++ config/cors.php
@@ -9,7 +9,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', '')))),
+    'allowed_origins' => array_filter(
+        array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', '')))
+    ),
 
     'allowed_origins_patterns' => [],
 
@@ -18,17 +20,28 @@ return [
         'Content-Type',
         'Authorization',
         'Origin',
+        'Referer',
         'X-Requested-With',
         'X-CSRF-TOKEN',
         'X-XSRF-TOKEN',
+
+        // Application access security headers
+        'X-Application-Password',
+        'X-App-Type',
+        'X-App-Origin',
+        'X-Debug-API-Client',
+
+        // Signature headers
         'X-Timestamp',
         'X-Nonce',
         'X-Signature',
     ],
 
-    'exposed_headers' => [],
+    'exposed_headers' => [
+        'Content-Type',
+    ],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
 
