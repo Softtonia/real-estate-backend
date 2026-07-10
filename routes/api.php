8a0959e3fa17d8534714d9601f2216283e28a055
vkwebdev <vijay.kumar@softtonia.com>
2026-07-10 15:02:02 +0530

    keywords

--- routes/api.php
+++ routes/api.php
@@ -920,7 +920,7 @@ Route::middleware(['throttle:60,1', 'admin.token', 'validate.api.client'])->grou
     Route::get('custom-fields', [DynamicPostController::class, 'customFieldsByPostType']);
     Route::get('dynamic-post-keyword-suggestions', [DynamicPostController::class, 'keywordSuggestions']);
 
-    Route::get('dynamic-post-user-assignment-options', [DynamicPostController::class, 'userAssignmentOptions']);
+    Route::get('dynamic-post-assignment-users', [DynamicPostController::class, 'assignmentUsers']);
     Route::get('dynamic-posts', [DynamicPostController::class, 'index']);
     Route::post('dynamic-posts', [DynamicPostController::class, 'store']);
     Route::get('dynamic-posts/{dynamicPost}', [DynamicPostController::class, 'show'])->whereNumber('dynamicPost');
