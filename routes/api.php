bca6c2229fc7ab7a01070729358e805f4401e54d
vkwebdev <vijay.kumar@softtonia.com>
2026-07-18 17:22:53 +0530

    user

--- routes/api.php
+++ routes/api.php
@@ -1134,9 +1134,9 @@ Route::middleware(['validate.api.client', 'throttle:60,1'])->group(function () {
         Route::middleware('auth:sanctum')->post('/', [FrontendListingController::class, 'store'])->name('store');
     });
     Route::prefix('frontend')->group(function () {
-    Route::get('frontend/locations/countries', [FrontendLocationController::class, 'countries']);
-    Route::get('frontend/locations/states', [FrontendLocationController::class, 'states']);
-    Route::get('frontend/locations/cities', [FrontendLocationController::class, 'cities']);
-    Route::get('frontend/locations/selected', [FrontendLocationController::class, 'selected']);
+    Route::get('locations/countries', [FrontendLocationController::class, 'countries']);
+    Route::get('locations/states', [FrontendLocationController::class, 'states']);
+    Route::get('locations/cities', [FrontendLocationController::class, 'cities']);
+    Route::get('locations/selected', [FrontendLocationController::class, 'selected']);
     });
 });
