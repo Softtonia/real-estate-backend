a58646f11fc4abb0545636f7fb609f8ca7da3a00
vkwebdev <vijay.kumar@softtonia.com>
2026-07-22 13:31:38 +0530

    user state

--- routes/api.php
+++ routes/api.php
@@ -957,8 +957,6 @@ Route::middleware(['throttle:60,1', 'admin.token', 'validate.api.client'])->grou
         ->whereNumber('dynamicPost');
     Route::put('dynamic-posts/{dynamicPost}', [DynamicPostController::class, 'update'])
         ->whereNumber('dynamicPost');
-    Route::post('dynamic-posts/{dynamicPost}', [DynamicPostController::class, 'update'])
-        ->whereNumber('dynamicPost');
     Route::delete('dynamic-posts/{dynamicPost}', [DynamicPostController::class, 'destroy'])
         ->whereNumber('dynamicPost');
 
